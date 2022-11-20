ac44a039c7eb60649c20d8c10dd129552cb0c271
nowadraco <Nowadraco@gmail.com>
2022-11-20 22:50:33 +0000

    testando javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const database = document.getElementById('database')
 
 const maxRecords = 151
 const limit = 8
@@ -45,3 +46,28 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function convertPokemonTodatabase(pokemondb) {
+    return `
+        <a  href="data-base.html">
+            <li class="pokemon ${pokemondb.type}">
+                <span class="number">#${pokemondb.number}</span>
+                <span class="name">${pokemondb.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemondb.photo}"
+                        alt="${pokemondb.name}">
+                </div>
+            </li>
+        </a>
+    `
+}
+
+function loadPokemondb() {
+    pokeApi.getPokemons().then((pokemonsdb = []) => {
+        const newHtml = pokemonsdb.map(convertPokemonTodatabase).join('')
+        database.innerHTML += newHtml
+    })
+}
