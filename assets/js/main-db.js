94b96bb49ad982486c44426e4c5a55457b64a4c4
nowadraco@gmail.com <nowadraco@gmail.com>
2023-01-29 20:26:26 -0300

    organizando semântica

--- assets/js/main-db.js
+++ assets/js/main-db.js
@@ -11,53 +11,48 @@ fetch(urlDetalhes)
 
 function convertPokemonDetalhes(pokemon) {
     return `
-    <section class="pokemondb ${pokemon.types[0].type.name}">
+      <section class="pokemondb ${pokemon.types[0].type.name}">
     <section class="nomenuberdb">
       <span class="namedb">${pokemon.name}</span>
       <span class="numberdb">#${pokemon.id}</span>
     </section>
     <ol class="typesdb">
-    ${pokemon.types.map(type => `<li class="typedb ${type.type.name}">${type.type.name}</li>`).join('')}
+      ${pokemon.types.map(type => `<li class="typedb ${type.type.name}">${type.type.name}</li>`).join('')}
     </ol>
     <section class="imagem">
       <div class="imgdb">
-        <img
-        src="${pokemon.sprites.other.dream_world.front_default}"
-        alt="${pokemon.name}"
-        />
+        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}" />
       </div>
     </section>
-    </section>
-    <section class="sobre">
+  </section>
+  <section class="sobre">
     <h3>Sobre</h3>
     <ol>
-    <li>Especie: ${pokemon.species.name}</li>
-    <li>Altura: ${pokemon.height/10} m</li>
-    <li>Peso: ${pokemon.weight/10} kg</li>
-    <li>Habilidades: ${pokemon.abilities.map(ability => ability.ability.name).join(', ')}</li>
+      <li>Especie: ${pokemon.species.name}</li>
+      <li>Altura: ${pokemon.height/10} m</li>
+      <li>Peso: ${pokemon.weight/10} kg</li>
+      <li>Habilidades: ${pokemon.abilities.map(ability => ability.ability.name).join(', ')}</li>
 
     </ol>
-    </section>
-    </section>
-    <section class="statusbase">
+  </section>
+  </section>
+  <section class="statusbase">
     <h3>Status Base</h3>
     <ol>
-    <li>HP: ${pokemon.stats[0].base_stat}</li>
-    <li>Ataque: ${pokemon.stats[1].base_stat}</li>
-    <li>Defesa: ${pokemon.stats[2].base_stat}</li>
-    <li>Especial Ataque: ${pokemon.stats[3].base_stat}</li>
-    <li>Especial Defesa: ${pokemon.stats[4].base_stat}</li>
-    <li>Velocidade: ${pokemon.stats[5].base_stat}</li>
-    <li>Total: ${pokemon.stats.reduce((acc, curr) => acc + curr.base_stat, 0)}</li>
+      <li>HP: ${pokemon.stats[0].base_stat}</li>
+      <li>Ataque: ${pokemon.stats[1].base_stat}</li>
+      <li>Defesa: ${pokemon.stats[2].base_stat}</li>
+      <li>Especial Ataque: ${pokemon.stats[3].base_stat}</li>
+      <li>Especial Defesa: ${pokemon.stats[4].base_stat}</li>
+      <li>Velocidade: ${pokemon.stats[5].base_stat}</li>
+      <li>Total: ${pokemon.stats.reduce((acc, curr) => acc + curr.base_stat, 0)}</li>
     </ol>
-    </section>
-    <section class="moves">
+  </section>
+  <section class="moves">
     <h3>Movimentos</h3>
     <ol class="movesl">
-        ${pokemon.moves.slice().map(move => `<li>${move.move.name}</li>`).join('')}
+      ${pokemon.moves.slice().map(move => `<li>${move.move.name}</li>`).join('')}
     </ol>
-</section>
-
-</section>
+  </section>
     `
 };
